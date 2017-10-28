5d11d27bc2f398d713c2a3b137dbf5659a4544ae
Vandhuy <vandhuy@gmail.com>
2017-10-28 08:01:53 -0300

    #1 Found a scaffold tool to start a react project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Assignment written in React
---
+++ config.js
@@ -0,0 +1,6 @@
+var configs = {};
+configs.applicationPort = 5000;
+configs.dbName = 'CrossoverVideosAssignment';
+configs.dbHost = 'localhost';
+
+module.exports = configs;
